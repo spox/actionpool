129a5bf95a6965f994155f7ea368eb02561da8b8
spox <spox@modspox.com>
2009-10-08 17:52:46 -0700

    send exceptions out of the pool

--- lib/actionpool/Thread.rb
+++ lib/actionpool/Thread.rb
@@ -5,14 +5,16 @@ module ActionPool
         # pool:: pool thread is associated with
         # t_timeout:: max time a thread is allowed to wait for action
         # a_timeout:: max time thread is allowed to work
+        # respond:: thread to send execptions to
         # logger:: LogHelper for logging messages
         # Create a new thread
-        def initialize(pool, t_timeout, a_timeout, logger=nil)
+        def initialize(pool, t_timeout, a_timeout, respond_thread, logger=nil)
             @pool = pool
             @pool_timeout = t_timeout.nil? ? 0 : t_timeout
             @action_timeout = a_timeout.nil? ? 0 : a_timeout
             @kill = false
             @logger = logger
+            @respond_to = respond_thread
             @thread = ::Thread.new{ start_thread }
         end
 
@@ -40,12 +42,14 @@ module ActionPool
                         run(action) unless action.nil?
                     rescue Timeout::Error => boom
                         @kill = true
-                    rescue Object => boom
+                    rescue Exception => boom
                         @logger.error("Pool thread caught an exception: #{boom}\n#{boom.backtrace.join("\n")}")
+                        @respond_to.raise boom
                     end
                 end
-            rescue Object => boom
+            rescue Exception => boom
                 @logger.error("Pool thread caught an exception: #{boom}\n#{boom.backtrace.join("\n")}")
+                @respond_to.raise boom
             ensure
                 @logger.info("Pool thread is shutting down (#{self})")
                 @pool.remove(self)
@@ -63,8 +67,6 @@ module ActionPool
                 end
             rescue Timeout::Error => boom
                 @logger.warn("Pool thread reached max execution time for action: #{boom}")
-            rescue Object => boom
-                @logger.error("Pool thread caught an exception running action: #{boom}\n#{boom.backtrace.join("\n")}")
             end
         end
     end
