48ceb56f8f7bf4897d17b23030de4156834723ff
Chris Roberts <chrisroberts.code@gmail.com>
2012-03-26 08:22:47 -0700

    Add version file

---
+++ lib/actionpool/version.rb
@@ -0,0 +1,17 @@
+module ActionPool
+  class Version
+
+    attr_reader :major, :minor, :tiny
+
+    def initialize(version)
+      version = version.split('.')
+      @major, @minor, @tiny = [version[0].to_i, version[1].to_i, version[2].to_i]
+    end
+
+    def to_s
+      "#{@major}.#{@minor}.#{@tiny}"
+    end
+  end
+
+  VERSION = Version.new('0.3.0')
+end
